29360b2d995e8901f89f3552e41f1a846627d63a
Ridwan Arifandi <orangerdigiart@gmail.com>
2025-09-12 08:17:07 +0700

    fix: variation swatches

--- assets/js/general.js
+++ assets/js/general.js
@@ -6,24 +6,29 @@
       $(".products[data-products] .product").each(function () {
         // check if element .ct-variation-swatches exists
         if ($(this).find(".ct-variation-swatches").length) {
-          // count how many .ct-swatch-container exists
-          var count = $(this).find(".ct-swatch-container").length;
-          // if count is more than 3 then add button "view more" inside .ct-variation-swatches and hide the rest;
-          if (count > 3) {
-            $(this)
-              .find(".ct-variation-swatches")
-              .append(
-                `<button class="view-more-variations">${count - 3}+</button>`
-              );
-            $(this).find(".ct-swatch-container:gt(2)").hide();
-          }
+          $(this)
+            .find(".ct-variation-swatches")
+            .each(function () {
+              // count how many .ct-swatch-container exists
+              var count = $(this).find(".ct-swatch-container").length;
+              // if count is more than 3 then add button "view more" inside .ct-variation-swatches and hide the rest;
+              if (count > 3) {
+                $(this).append(
+                  `<button class="view-more-variations">${count - 3}+</button>`
+                );
+                // $(this).find(".ct-swatch-container:gt(2)").hide();
+              }
+            });
         }
       });
     }
 
     $(document).on("click", ".view-more-variations", function () {
       $(this).hide();
-      $(this).parent().find(".ct-swatch-container:gt(2)").show();
+      $(this)
+        .parent()
+        .find(".ct-swatch-container:gt(2)")
+        .css("display", "flex");
     });
   });
 })(jQuery);
