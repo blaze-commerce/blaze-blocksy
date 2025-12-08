1e456a0285cf1fcf80cebb4ecf834e139cc63f65
Ridwan Arifandi <orangerdigiart@gmail.com>
2025-12-08 11:34:04 +0700

    fix: remove unneeded script

--- functions.php
+++ functions.php
@@ -262,33 +262,3 @@ add_filter( 'woocommerce_locate_template', function ( $template, $template_name 
 	return $template;
 
 }, 999, 2 );
-
-/**
- * Custom code for infinitytargets only
- */
-
-add_action( 'template_redirect', function () {
-
-
-	if ( ! is_page( 'dealer-resources' ) ) {
-		return true;
-	}
-
-	// check if current user role is admin or editor
-	if ( current_user_can( 'administrator' ) || current_user_can( 'editor' ) ) {
-		return true;
-	}
-
-	if ( is_user_logged_in() ) {
-
-		$user_id = get_current_user_id();
-		if ( function_exists( 'is_wholesaler_user' ) && is_wholesaler_user( $user_id ) ) {
-			return true;
-		}
-
-	}
-
-	//redirect user to home page
-	wp_redirect( home_url() );
-	exit;
-} );
