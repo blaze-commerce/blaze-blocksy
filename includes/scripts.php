6820834534de44288e944234f264f84579eb97d9
Lan <alan@blazecommerce.io>
2026-01-22 13:35:50 +0800

    fix: Add version parameter to checkout CSS enqueue for cache busting

--- includes/scripts.php
+++ includes/scripts.php
@@ -89,7 +89,7 @@ function blaze_blocksy_enqueue_assets() {
 	// === CHECKOUT PAGE ASSETS ===
 	if ( is_checkout() ) {
 		// Checkout page styles
-		wp_enqueue_style( 'blaze-blocksy-checkout', BLAZE_BLOCKSY_URL . '/assets/css/checkout.css' );
+		wp_enqueue_style( 'blaze-blocksy-checkout', BLAZE_BLOCKSY_URL . '/assets/css/checkout.css', array(), '1.0.0' );
 	}
 
 	// === WISHLIST OVERLAY ASSETS ===
